c2d2a1ba05b4488125717803bf520a5316f1feca
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-04 20:30:59 +0800

    使用前置守卫验证登录功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login.vue'
+import Personal from '@/views/personal.vue'
 Vue.use(VueRouter)
 let router = new VueRouter({
   routes: [
@@ -8,7 +9,28 @@ let router = new VueRouter({
       name: 'Login',
       path: '/login',
       component: Login
+    },
+    {
+      name: 'Personal',
+      path: '/personal',
+      component: Personal
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  // console.log(to);
+  // console.log(from);
+  if (to.path.indexOf('/personal') === 0) {
+    let token = localStorage.getItem('user_token')
+    if (token) {
+      next()
+    } else {
+      next({ path: '/login' })
+    }
+  } else {
+    next()
+
+  }
+})
+
 export default router
